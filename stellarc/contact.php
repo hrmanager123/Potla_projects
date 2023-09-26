90079db57fad0742648fc0f4550e093824c015c1
mohan9888 <mohan.m@potlaservices.com>
2023-09-26 11:15:02 +0530

    searchbar color changes

---
+++ stellarc/contact.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+   
+    $message = $_POST["message"];
+
+    
+    $to = "support@stellar-cs.com";
+    $subject = "Contact Form Submission";
+    $message = "Name:$name\nPhone:$phone\nEmail:$email\nMessage:$message";
+    $headers = "From:support@stellar-cs.com"; 
+    $mailSent = mail($to, $subject, $message, $headers);
+
+    if ($mailSent) {
+       header("Location:contactus.html");
+    } else {
+        echo "error";
+        
+        
+    }
+}
+?>
